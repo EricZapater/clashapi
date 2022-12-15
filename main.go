63eab5ca81cf76955cde7da02b79643e3100a4b2
Èric David Zapater Montorio <ezapaterm@gmail.com>
2022-12-15 13:18:27 +0100

    Add get for public ip

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/EricZapater/clashapi/environment"
 	"github.com/EricZapater/clashapi/service"
 )
 
+type IP struct {
+	Query string
+}
+
+func getip2() string {
+	req, err := http.Get("http://ip-api.com/json/")
+	if err != nil {
+		return err.Error()
+	}
+	defer req.Body.Close()
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return err.Error()
+	}
+
+	var ip IP
+	json.Unmarshal(body, &ip)
+
+	return ip.Query
+}
+
 func main() {
+	ip := getip2()
+	fmt.Println(ip)
 	for {
 		env := environment.LoadEnvironment()
 		zona, _ := time.Now().Zone()
